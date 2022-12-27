9b46aae3afc4e8c5a7661f0a983f95e30dfadadc
Sandhya Ratan <Sandhya.Ratan@philips.com>
2022-12-27 18:00:27 +0530

    path problems solved with dfs

---
+++ Tree/lowest-common-ancestor-of-the-deepest-leaves-of-a-binary-tre/Program.cs
@@ -0,0 +1,77 @@
+﻿using Common;
+using System;
+
+namespace lowest_common_ancestor_of_the_deepest_leaves_of_a_binary_tre
+{
+    class Program
+    {
+        static int findDepth(Node root)
+        {
+            if (root == null)
+                return 0;
+
+            int left = findDepth(root.LeftNode);
+
+            int right = findDepth(root.RightNode);
+
+            return 1 + Math.Max(left, right);
+        }
+
+        static Node DFS(Node root, int curr,
+                           int depth)
+        {
+
+            if (root == null)
+                return null;
+
+            // If curr is equal to depth
+            if (curr == depth)
+                return root;
+
+            // Left recursive subtree
+            Node left = DFS(root.LeftNode, curr + 1, depth);
+
+            // Right recursive subtree
+            Node right = DFS(root.RightNode, curr + 1, depth);
+
+            // If left and right are not null
+            if (left != null && right != null)
+                return root;
+
+            // Return left, if left is not null
+            // Otherwise return right
+            return (left != null) ? left : right;
+        }
+
+        static Node lcaOfDeepestLeaves(Node root)
+        {
+
+            // If root is null
+            if (root == null)
+                return null;
+
+            // Stores the deepest depth
+            // of the binary tree
+            int depth = findDepth(root) - 1;
+
+            // Return the LCA of the
+            // nodes at level depth
+            return DFS(root, 0, depth);
+        }
+
+        static void Main(string[] args)
+        {
+            Node root = new Node(3);
+            root.LeftNode = new Node(5);
+            root.RightNode = new Node(1);
+            root.LeftNode.LeftNode = new Node(6);
+            root.LeftNode.RightNode = new Node(2);
+            root.RightNode.LeftNode = new Node(0);
+            root.RightNode.RightNode = new Node(8);
+            root.LeftNode.RightNode.LeftNode = new Node(7);
+            root.LeftNode.RightNode.RightNode = new Node(4);
+
+            Console.WriteLine(lcaOfDeepestLeaves(root).data);
+        }
+    }
+}
---
+++ Tree/maximum-depth-or-height-of-a-tree/Program.cs
@@ -0,0 +1,36 @@
+﻿using Common;
+using System;
+
+namespace maximum_depth_or_height_of_a_tree
+{
+    class Program
+    {
+        Node root;
+        static void Main(string[] args)
+        {
+            Program tree = new Program();
+
+            tree.root = new Node(1);
+            tree.root.LeftNode = new Node(2);
+            tree.root.RightNode = new Node(3);
+            tree.root.LeftNode.LeftNode = new Node(4);
+            tree.root.LeftNode.RightNode = new Node(5);
+
+            Console.WriteLine("Height of tree is "
+                              + tree.maxDepth(tree.root));
+        }
+
+        private int maxDepth(Node root)
+        {
+            if (root == null)
+                return 0;
+            else
+            {
+                int LeftLength = maxDepth(root.LeftNode);
+                int rightlength = maxDepth(root.RightNode);
+                return Math.Max(LeftLength, rightlength) + 1;
+            }
+
+        }
+    }
+}
